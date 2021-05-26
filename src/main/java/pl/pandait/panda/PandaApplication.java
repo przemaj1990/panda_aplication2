3c1e597f9c3c490949b72e59cce3af4554f0cf17
przemaj1990 <przemaj1990@gmail.com>
2021-05-26 16:15:19 +0000

    po kopiowaniu

---
+++ src/main/java/pl/pandait/panda/PandaApplication.java
@@ -0,0 +1,12 @@
+package pl.pandait.panda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PandaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PandaApplication.class, args);
+	}
+}
